16912fd85ad229feb8348917e4aa097a1cb26f66
Thiago Gabriel Torres <161034897+Thiago45367@users.noreply.github.com>
2024-09-04 11:55:54 +0000

    python-4

---
+++ 2.Atividade_1.py
@@ -0,0 +1,25 @@
+import os
+
+os .system("cls || clear")
+
+class Pet:
+
+    def __init__(self, nome: str, idade: int, raca: str, porte: str, alimentacao: str) -> None:
+         self.nome: str = nome
+         self.idade: int = idade
+         self.raca: str = raca
+         self.porte: str = porte
+         self.alimentacao: str = alimentacao
+
+    def exibir_dados(self) -> str:
+          return (f"Nome: {self.nome}"
+                 f"\nIdade: {self.idade}"
+                 f"\nRaça: {self.raca}"
+                 f"\nPorte: {self.porte}"
+                 f"\nAlimentação: {self.alimentacao}"
+                  )
+
+
+pet = Pet("hercules", 2, "chow-chow", "médio", "ração")
+
+print(pet.exibir_dados())
